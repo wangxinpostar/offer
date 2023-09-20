8b0703fb61eff98177be099eb1124edfd3c1a55d
wangxinpo <1124434059@qq.com>
2023-09-20 09:12:00 +0800

    黑马点评解决分布式线程安全问题（抢购优惠卷）基于Lua脚本 解决锁误删问题

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,14 +1,25 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     private final String name;
     private final RedisTemplate<String, String> redisTemplate;
 
@@ -28,6 +39,10 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
+
+//        调用lua脚本
+        redisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(KEY_PREFIX + name), ID_PREFIX + Thread.currentThread().getId());
+
 //        String threadId = ID_PREFIX + Thread.currentThread().getId();
 //        String id = redisTemplate.opsForValue().get(KEY_PREFIX + name);
 //        if (threadId.equals(id)) {
